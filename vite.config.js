f6f5b79f450a413397cbe5f101993447afcc0738
aayu22809 <aayu22809@gmail.com>
2026-01-24 08:41:12 -0800

    fix vite issue

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/thinkTwice/',
   plugins: [react()],
   test: {
-    base: '/thinkTwice/',
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
